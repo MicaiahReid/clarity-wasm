61d82783e6775d640387c6ef1b1369fb7b510ada
Chris Guimaraes <cguimaraes@hiro.so>
2024-02-19 11:03:03 +0000

    tests: more crossprop tests to map functions

--- clar2wasm/tests/wasm-generation/map.rs
+++ clar2wasm/tests/wasm-generation/map.rs
@@ -3,7 +3,7 @@ use clarity::vm::types::{ListTypeData, SequenceSubtype, StringSubtype, TypeSigna
 use proptest::strategy::{Just, Strategy};
 use proptest::{prop_oneof, proptest};
 
-use crate::{list, PropValue, Value};
+use crate::{list, uint, PropValue, TypePrinter, Value};
 
 fn strategies_for_list() -> impl Strategy<Value = TypeSignature> {
     prop_oneof![
@@ -66,5 +66,57 @@ proptest! {
         let snippet = &format!("(and (fold or (map not (list (is-eq {vals} {vals}) (is-eq {vals} {vals}))) true))");
 
         crosscheck(snippet, Ok(Some(Value::Bool(true))));
+    }
+}
+
+proptest! {
+    #![proptest_config(super::runtime_config())]
+
+    #[test]
+    fn crossprop_datamap_insert(key in uint(), vals in PropValue::any_sequence(20usize)) {
+        let vals_type_str = vals.type_string();
+        crosscheck_compare_only(
+            &format!(
+                r#"
+                  (define-map mp {{x: uint}} {{y: {vals_type_str} }})
+                  (map-insert mp {{x: {key}}} {{y: {vals}}})
+                "#
+            )
+        )
+    }
+}
+
+proptest! {
+    #![proptest_config(super::runtime_config())]
+
+    #[test]
+    fn crossprop_datamap_set(key in uint(), vals in PropValue::any_sequence(20usize)) {
+        let vals_type_str = vals.type_string();
+        crosscheck_compare_only(
+            &format!(
+                r#"
+                  (define-map mp {{x: uint}} {{y: {vals_type_str} }})
+                  (map-set mp {{x: {key}}} {{y: {vals}}})
+                "#
+            )
+        )
+    }
+}
+
+proptest! {
+    #![proptest_config(super::runtime_config())]
+
+    #[test]
+    fn crossprop_datamap_delete(key in uint(), vals in PropValue::any_sequence(20usize)) {
+        let vals_type_str = vals.type_string();
+        crosscheck_compare_only(
+            &format!(
+                r#"
+                  (define-map mp {{x: uint}} {{y: {vals_type_str} }})
+                  (map-set mp {{x: {key}}} {{y: {vals}}})
+                  (map-delete mp {{ x: {key} }})
+                "#
+            )
+        )
     }
 }
